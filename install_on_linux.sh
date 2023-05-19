442c6e4f16e03928f820c9c572f94eac9b620b0a
Is Ma <dotpointing@gmail.com>
2023-05-19 14:40:41 -0600

    Repair aliaz

--- install_on_linux.sh
+++ install_on_linux.sh
@@ -1,7 +1,6 @@
 cat << EOF >> ~/.bashrc
 
 ### IS-MA: ALIAZ (Bash) ### [type 'alib' to list them all]
-source ~/.is-ma/aliaz/mods/welcome.sh
 source ~/.is-ma/aliaz/mods/linux_only.sh
 source ~/.is-ma/aliaz/mods/any_os.sh
 EOF
--- install_on_macos.sh
+++ install_on_macos.sh
@@ -1,7 +1,6 @@
 cat << EOF >> ~/.bash_profile
 
  ### IS-MA: ALIAZ (Mac OS) ### [type 'alim' to list them all]
- source ~/.is-ma/aliaz/mods/welcome.sh
  source ~/.is-ma/aliaz/mods/mac_only.sh
  source ~/.is-ma/aliaz/mods/any_os.sh
 EOF
